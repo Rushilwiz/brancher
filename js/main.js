2a663a610cc1a317f3e7893011d5fa540c27a539
rushilwiz <rushilwiz@gmail.com>
2020-03-18 23:01:08 -0400

    began work

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-// Sticky Header
-$(window).scroll(function() {
-
-    if ($(window).scrollTop() > 100) {
-        $('.main_h').addClass('sticky');
-    } else {
-        $('.main_h').removeClass('sticky');
-    }
-});
 
 // Mobile Navigation
 $('.mobile-toggle').click(function() {
