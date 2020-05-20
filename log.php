97ebf395989f41838d000599552d7ff2aff82431
1776123 <50336512+1776123@users.noreply.github.com>
2020-05-19 21:28:42 -0400

    beginning of login php

---
+++ log.php
@@ -0,0 +1,13 @@
+<!--link href='https://fonts.googleapis.com/css?family=Open+Sans:700,600' rel='stylesheet' type="text/css">
+link rel="stylesheet" type="text/css" href="css/login.css"-->
+<html>
+  <head>
+    <title>Information Gathered</title>
+  </head>
+  <body>
+    <?php
+      echo "<p>Data processed</>";
+      data_default_timezone_set('UTC');
+    ?>
+  </body>
+</html>
